d0100d5e645ca73a7ad583991179657407f5736c
Preston Williams <84547105+xNightmxreZ@users.noreply.github.com>
2026-01-13 13:02:18 -0500

    continued home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,29 @@
 import Link from "next/link";
+import Image from 'next/image';
+import Logo from '../assets/cornell_logo.png';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-white font-sans">
-      <nav className="flex w-full justify-end items-center px-4 text-black text-2xl">
-        <div className="flex justify-center gap-4">
+    <div className="area min-h-screen bg-white text-black font-sans p-2">
+      <nav className="flex w-full justify-end p-4 text-2xl">
+        <div className="flex gap-4">
           <Link href={'/signup'}>Sign Up</Link>
           <div className="mb-1">|</div>
           <Link href={'/login'}>Log In</Link>
         </div>
       </nav>
-      <div>
-
+      <div className="flex items-center justify-center gap-10 mt-10">
+        <div className="flex justify-center text-left flex-col gap-4">
+          <div>
+            <h2 className="text-4xl font-bold">Big Red Notes</h2>
+            <p className="text-xl font-bold">Cornell's Premier Note Sharing Platform</p>
+          </div>
+          <p className="text-xl">Sign up with your Cornell email to access!</p>
+          <p className="text-xl">Contribute to the community and submit your old notes!</p>
+        </div>
+        <div className="flex">
+          <Image src={Logo} alt={"Cornell Logo"} className="max-h-60 w-auto" />
+        </div>
       </div>
     </div>
   );
